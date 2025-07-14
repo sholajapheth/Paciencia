1cd99f5b283140df8cc3aad33dfe94daab8ce893
Shola Japheth <sholajapheth@gmail.com>
2025-07-14 03:48:52 -0700

    fix: an

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import { SiteFooter } from '@/components/site-footer';
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col">
-      <header className="container mx-auto px-4 sm:px-6 lg:px-8 py-4">
+      <header className="container mx-auto px-4 sm:px-6 lg:px-8 py-4 bg-white flex items-center ">
         <Logo />
       </header>
 
--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,13 +1,14 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 export function Logo() {
   return (
     <Link
       href="/"
-      className="inline-block font-headline text-2xl tracking-tight"
+      className="inline-block font-headline text-2xl tracking-tight  items-center justify-center"
       aria-label="Paciencia Home"
     >
-      Paciencia
+      <Image src="/logo_p.png" alt="Paciencia" className='w-[150px] md:w-[170px] h-auto' width={100} height={100} />
     </Link>
   );
 }
